05963f5cd4a561052287b3f67977c0aaa75503c6
Edwin Tsafack Nkonzong <tsafackedwin@gmail.com>
2025-06-19 01:09:23 -0400

    Created basic website

--- Visualcodesite/wwwroot/js/site.js
+++ Visualcodesite/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let title = document.getElementById("animatedTitle");
+let direction = 1;
+let position = 0;
+
+function animateTitle() {
+    if (position >= 20 || position <= -20) direction *= -1;
+    position += direction;
+    title.style.transform = `translateX(${position}px)`;
+    requestAnimationFrame(animateTitle);
+}
+
+animateTitle();
